434da0053df916f4ed8fc754af44bef465feedbd
ssebastianoo <sebastianogirotto05@gmail.com>
2022-07-12 16:45:40 +0200

    send right data

--- pages/create.jsx
+++ pages/create.jsx
@@ -43,7 +43,11 @@ export default function Create() {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({
-                article: data,
+                article: {
+                    title: e.target.title.value,
+                    url: url,
+                },
+                author: supabase.auth.user().username,
             }),
         });
         location.href = "/articles/" + url;
