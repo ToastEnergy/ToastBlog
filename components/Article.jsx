be1db8727f1975e8386e520ca6a2ac6462d552f9
ssebastianoo <sebastianogirotto05@gmail.com>
2022-07-27 19:47:14 +0200

    datetime

--- components/Article.jsx
+++ components/Article.jsx
@@ -12,6 +12,18 @@ export default function Article({ article, preview = false }) {
         button = true;
     }
 
+    function formatDate(date) {
+        const d = new Date(date);
+        let res = d.getFullYear();
+        if ((d.getMonth() + 1) < 10) {
+            res += "-0" + (d.getMonth() + 1);
+        } else {
+            res += "-" + (d.getMonth() + 1);
+        }
+        res += "-" + d.getDate();
+        return res;
+    }
+
     return (
         <article>
             <div className={"article" + (preview ? " preview" : " full")}>
@@ -24,9 +36,12 @@ export default function Article({ article, preview = false }) {
                         </a>
                     </Link>
                 ) : null}
-                <p className="date">
+                <time
+                    dateTime={formatDate(article.created_at)}
+                    className="date"
+                >
                     {new Date(article.created_at).toDateString()}
-                </p>
+                </time>
                 <p className="author">
                     <Link href={"/users/" + article.users.username}>
                         <a>@{article.users.username}</a>
